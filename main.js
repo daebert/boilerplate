65d3afa06e4a8e394d0d07c13e7d2e50d4c4f108
daniel.ebert <daniel.ebert@web.de>
2021-05-03 15:56:42 +0200

    Eslint config

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML =""
+document.querySelector('#app').innerHTML =
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>;
+
+  
