9d0beda5b34ca83d16df07ce751b03526656b45a
daniel.ebert <daniel.ebert@web.de>
2021-06-07 10:23:00 +0200

    :construction_worker: Serve storybook

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,6 @@ const app = express();
 
 // Serve app production bundle
 app.use(express.static("dist/app"));
+
+// Serve storybook production bundle
+app.use("/storybook", express.static("dist/storybook"));
